beb23463efb083279ffd5925511e4207a1e4195f
daanishvirani <daanishv@gmail.com>
2019-05-10 15:39:24 -0500

    Inital UX changes

--- react/src/components/AddClientForm.js
+++ react/src/components/AddClientForm.js
@@ -1,23 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, label, Input, FormText} from 'reactstrap';
 
 export default class AddClientForm extends Component {
   render() {
     return (
-      <form onSubmit={this.props.addClient}>
-        <label>
-          name
-          <input name="name" />
-        </label>
-        <label>
-          address
+      <Form onSubmit={this.props.addClient}>
+        <FormGroup>
+          <label>Name</label>
+          <input name = "name" />
+        </FormGroup>
+        <FormGroup>
+          <label>Address</label>
           <input name="address" />
-        </label>
-        <label>
-          phone number
+        </FormGroup>
+        <FormGroup>
+          <label>Phone Number</label>
           <input name="phoneNumber" />
-        </label>
-        <input type="submit"/>
-      </form>
+        </FormGroup>
+        <FormGroup>
+          <input type="submit" />
+        </FormGroup>
+      </Form>
+
+      
+      // <form onSubmit={this.props.addClient}>
+      //   <label>
+      //     name
+      //     <input name="name" />
+      //   </label>
+      //   <label>
+      //     address
+      //     <input name="address" />
+      //   </label>
+      //   <label>
+      //     phone number
+      //     <input name="phoneNumber" />
+      //   </label>
+      //   <input type="submit"/>
+      // </form>
+      
     )
   }
 }
--- react/src/components/PetList.js
+++ react/src/components/PetList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Table } from 'reactstrap';
 
 export default class PetsList extends Component {
   state = {
@@ -43,7 +44,7 @@ export default class PetsList extends Component {
     // need to create table and populate
 
     return (
-      <table>
+      <Table hover>
         <thead>
           <tr>
             <th>Id</th>
@@ -53,7 +54,7 @@ export default class PetsList extends Component {
         </thead>
 
         <this.ShowAllPets />
-      </table>
+      </Table>
     );
   }
 }
